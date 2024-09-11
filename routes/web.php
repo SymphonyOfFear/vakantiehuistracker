f830ffac1f9b30e302a12006eb17de6604a5af44
SymphonyOfFear <124155888+SymphonyOfFear@users.noreply.github.com>
2024-09-11 11:21:49 +0200

    adada

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welkom');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
