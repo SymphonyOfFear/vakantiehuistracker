7aedd8bb9bd1251d8350ecb01799b2b9d9189508
SymphonyOfFear <124155888+SymphonyOfFear@users.noreply.github.com>
2024-09-18 13:32:38 +0200

    Fixed Routes to profile

--- routes/web.php
+++ routes/web.php
@@ -54,9 +54,10 @@ Route::middleware('auth')->group(function () {
 
 // Profile Routes
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit'); // Bewerk profiel
-    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update'); // Update profiel
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy'); // Verwijder profiel
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 // Recensies (Feedback) Routes
