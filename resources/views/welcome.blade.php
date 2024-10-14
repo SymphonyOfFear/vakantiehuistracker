d2f19fe1310a3931076c9b483745adc07a4e5de2
finn <finn@basensas.nl>
2024-10-14 14:42:08 +0200

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,7 +54,7 @@
                                         <h3 class="text-xl font-bold text-gray-800">{{ $huisje->naam }}</h3>
                                         <p class="text-gray-600">Locatie: {{ $huisje->locatie }}</p>
                                         <p class="text-green-600 font-semibold">€ {{ $huisje->prijs }}</p>
-                                        <a href="{{ route('verhuurder.huizen.store', $huisje->id) }}"
+                                        <a href="{{ route('verhuurder.huizen.store ', $huisje->id) }}"
                                             class="mt-4 inline-block bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 transition">
                                             Bekijk details
                                         </a>                                   
